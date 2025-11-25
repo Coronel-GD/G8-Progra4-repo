7a1e8c984508d2f2aaee6c06f1db21849272411f
Gaston <g.coronel2005@gmail.com>
2025-11-25 01:30:30 -0300

    Fix migration dependency chain after removing PostGIS migration

--- core/migrations/0009_order_shipping_cost.py
+++ core/migrations/0009_order_shipping_cost.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0008_userprofile_location'),
+        ('core', '0007_alter_item_id_alter_order_id_alter_orderitem_id_and_more'),
     ]
 
     operations = [
